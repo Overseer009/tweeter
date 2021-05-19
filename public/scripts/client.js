967814ca4a8fea09042b86ebdf10224501218006
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-19 14:20:21 -0400

    added the loadTweet and the page refresher

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,35 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
 
   $('#tweet-messenger').submit(function( event ) {
-    // console.log(event);
     event.preventDefault();
-
-    console.log($(this).serialize());
-
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: $(this).serialize()
     }).then((data) => {
-      console.log('data:', data);
-    }).catch((error) => {
-      console.log("error:", error);
+      location.reload()
     })
-    
   });
   
+  const loadTweets = () => {
+    $.ajax({
+      method: "GET",
+      url: "/tweets"
+    }).then((otherTweets) => {
+      renderTweets(otherTweets);
+      
+      timeago.render(document.querySelectorAll('.time'));
+    })
+  }
+
+  loadTweets()
+  
+
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let newTweet = createTweetElement(tweet);
       console.log(newTweet);
-      $('#tweets-container').append(newTweet);
+      $('#tweets-container').prepend(newTweet);
     }
   };
 
@@ -55,31 +56,4 @@ $(document).ready(function () {
     `);
     return $tweet;
   };
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  renderTweets(data);
 })
