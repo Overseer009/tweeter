e8c829cb8932f65de0465d6d9b9623dae2d3be50
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-19 11:22:02 -0400

    added the renderTweet function to loop through the tweets and display their containers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,44 +5,63 @@
  */
 
 $(document).ready(function () {
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let newTweet = createTweetElement(tweet);
+      console.log(newTweet);
+      $('#tweets-container').append(newTweet);
+    }
+  };
+
   const createTweetElement = (tweet) => {
-  return $(`
-  <article>
-  <header>
-    <div class="user">
-      <img src="${tweet.user.avatars}"> 
-      <span>"${tweet.user.name}"</span>
-    </div>
-    <span class="tag">"${tweet.user.handle}"</span>
-  </header>
-    <h5 class="text">"${tweet.content.text}"</h5>
-  <footer>
-    <span class="time" datetime="${tweet.created_at}"></span>
-    <div class="interactive">
-      <i class="fas fa-flag flag"></i>
-      <i class="fas fa-retweet share"></i>
-      <i class="fas fa-heart love"></i>
-    </div>
-  </footer>
-</article>
-  `);
-};
+    let $tweet = $(`
+    <article>
+      <header>
+        <div class="user">
+          <img src="${tweet.user.avatars}"> 
+          <span>"${tweet.user.name}"</span>
+        </div>
+        <span class="tag">"${tweet.user.handle}"</span>
+      </header>
+        <h5 class="text">"${tweet.content.text}"</h5>
+      <footer>
+        <span class="time" datetime="${tweet.created_at}"></span>
+        <div class="interactive">
+          <i class="fas fa-flag flag"></i>
+          <i class="fas fa-retweet share"></i>
+          <i class="fas fa-heart love"></i>
+        </div>
+      </footer>
+    </article>
+    `);
+    return $tweet;
+  };
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const $tweet = createTweetElement(tweetData)
-
-console.log($tweet);
-
-$('#tweets-container').append($tweet);
+  renderTweets(data);
 })
