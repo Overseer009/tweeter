165077aa2a43e2fb4d89d71ca1fae11e2c83fe0e
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-19 11:02:59 -0400

    got the tweeter container to append to the index html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const createTweetElement = (tweet) => {
-  const $tweet = $(`
+$(document).ready(function () {
+  const createTweetElement = (tweet) => {
+  return $(`
   <article>
   <header>
     <div class="user">
@@ -37,8 +26,23 @@ const createTweetElement = (tweet) => {
   </footer>
 </article>
   `);
-  return $tweet;
 };
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData)
 
 console.log($tweet);
 
+$('#tweets-container').append($tweet);
+})
